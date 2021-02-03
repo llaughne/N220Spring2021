c2ebd551d7776cd8de32eb7a0f9ab95f8811e385
llaughne <logan32790@yahoo.com>
2021-02-02 22:00:11 -0500

    added lab 2 exercises 1 and 2

---
+++ Labs/Lab2/Exercise 1/js/app.js
@@ -0,0 +1,5 @@
+let dog = 0;
+function draw() {
+    dog = dog + 1;
+    console.log(dog);
+}
---
+++ Labs/Lab2/Exercise 2/js/app.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 300);
+  }
+  
+  function draw() {
+  background(255);
+    
+  if(mouseX>200)
+  {
+  fill("#e60707");
+  circle(mouseX,mouseY,50);
+  }
+  else
+  {
+  fill("#1207e6");
+  circle(mouseX,mouseY,50);
+  }
+  }
