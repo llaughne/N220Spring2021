0203a2f9d68d9722d8de43625831d4dd31b1ea59
llaughne <logan32790@yahoo.com>
2021-02-09 23:15:23 -0500

    lab 3 exercise 2 added

--- Labs/Lab3/Exercise 2/js/app.js
+++ Labs/Lab3/Exercise 2/js/app.js
@@ -1,1 +1,21 @@
+let i = 3;
 
+function setup() {
+    createCanvas(800, 600);
+}
+
+function draw() {
+    background(255);
+    fill(255, 0, 0);
+  
+    
+    for (let row = 0; row <= i; row++) {
+      
+        
+        for (let column = 0; column <= row ; column ++) {
+           square(column * 50, row * 50, 50);
+        }
+    }
+}
+
+    
